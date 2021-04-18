ad9f636c7b6cfe41ccccaaa44417ee666bc8af23
tom <leko.tomas@fronius.com>
2021-04-18 07:34:03 +0200

    try making invalid state unrepresentable

--- src/FifteenPuzzle.fs
+++ src/FifteenPuzzle.fs
@@ -2,62 +2,69 @@ module FifteenPuzzle
 
 open System
 
-type Position = { X: int; Y: int }
+type Position = { Row: int; Col: int }
 
 type Slot = Position * string
 
-type AppState = { Slots : Slot list;  FreePos : Position }
+let [<Literal>] RowCount = 4
+let [<Literal>] RowLength = 4
+let FreeTag = RowCount*RowLength
+
+let indexToPosition (index: int) =
+    { Row = index / RowLength; Col = index % RowLength }
+
+let positionToIndex (position: Position) =
+    position.Row*RowLength + position.Col
+
+//type AppState = { Slots : Slot list;  FreePos : Position }
+type AppState = 
+    { Tags: int [] } 
+    member appState.FreePos =
+        appState.Tags
+        |> Array.findIndex ((=) FreeTag)
+        |> indexToPosition
+    member appState.Slots =
+        appState.Tags
+        |> Array.mapi (fun index tag ->
+            indexToPosition index, string tag)
 
 let random = Random()
 
 let initialState() : AppState =
-    let randomTags = List.sortBy (fun _ -> random.NextDouble()) [1 .. 16]
-    // generate slot positions
-    [
-        for x in 0 .. 3 do
-        for y in 0 .. 3 do
-        yield { X = x; Y = y }  ]
-    // give each position a random tag, making it a slot
-    |> List.mapi (fun i pos -> pos, string (List.item i randomTags))
-    |> fun slots ->
-        // find the free slot, it has tag "16"
-        let (pos, _) = Seq.find (fun (p, tag) -> tag = "16") slots
-        // return initial state
-        { Slots = slots; FreePos = pos }
+    //{ Tags = [|1..FreeTag|] } // Solved State for testing
+    { Tags = Array.sortBy (fun _ -> random.NextDouble()) [|1 .. FreeTag|] }
 
 let freePositionTag (state:AppState) =
     state.Slots
-    |> List.find (fun (pos, t) -> pos = state.FreePos)
+    |> Array.find (fun (pos, t) -> pos = state.FreePos)
     |> snd
 
-let slotSelected (state:AppState)  (position: Position) (tag: string) =
-    { state with
-        FreePos = position
-        Slots =
-            if position = state.FreePos
-            then state.Slots
-            else
-                state.Slots
-                |> List.map (fun (slotPosition, slotTag) ->
-                    if slotPosition = state.FreePos
-                    then slotPosition, tag
-                    else if slotPosition = position
-                    then
-                        let oldFreePositionTag = freePositionTag state
-                        slotPosition, oldFreePositionTag
-                    else slotPosition, slotTag) }
-
-let stylesheet = Stylesheet.load "./fitteen-puzzle.module.css"
-
-let canMove (state:AppState) (position: Position)  =
-    let { X = x1; Y = y1 } = position
-    let { X = x2; Y = y2 } = state.FreePos
+let canMove (state: AppState) (position: Position)  =
+    let { Row = x1; Col = y1 } = position
+    let { Row = x2; Col = y2 } = state.FreePos
     let xDiff = abs (x2 - x1)
     let yDiff = abs (y2 - y1)
     xDiff + yDiff <= 1
 
+let slotSelected (state:AppState) (position: Position) (tag: string) =
+    let tag = int tag
+    if canMove state position 
+    then 
+        let tags' = 
+            state.Tags
+            |> Array.map (fun t ->
+                if t = FreeTag 
+                then tag
+                elif t = tag
+                then FreeTag
+                else t )
+        { state with Tags = tags' }
+    else state
+
+let stylesheet = Stylesheet.load "./fitteen-puzzle.module.css"
+
 let inFinalPosition (position: Position) (tag: string) =
-    let { X = x; Y = y } = position
+    let { Row = x; Col = y } = position
     (x * 4) + y + 1 = int tag
 
 let gameFinished (state: AppState) =
